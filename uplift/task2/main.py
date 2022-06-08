d4d6b8a06500db3619c0baef1063db08a148c413
fed <fedor.fomichev@phystech.edu>
2022-06-08 10:59:21 +0300

    Task2 done!

--- uplift/task2/main.py
+++ uplift/task2/main.py
@@ -10,21 +10,15 @@ def read_data(cfg):
     target = read_np_array(cfg.target)
     treatment = read_np_array(cfg.treatment)
     forecast = read_np_array(cfg.forecast)
-    print(f'features shape: {features.shape}')
-    print(f'treatment unique cnt: {len(np.unique(treatment))}')
+
     return features, target, treatment, forecast
 
 
 def _check(model_constructor, model_params, X, treatment, y, X_test, pred_right, eps=1e-5) -> bool:
     model = model_constructor(**model_params)
-    print('Start fitting')
     model.fit(X, treatment, y)
-    print('Start inference')
     pred = np.array(model.predict(X_test)).reshape(len(X_test))
-    print(f'predict mean: {pred.mean()}')
-    print(f'predict right: {pred_right.mean()}')
     score = np.max(np.abs(pred - pred_right))
-    print(score)
     passed = (score < eps)
 
     return passed
--- uplift/task2/tree.py
+++ uplift/task2/tree.py
@@ -1,9 +1,13 @@
-from typing import Iterable
+from typing import Iterable, Tuple
 
 import numpy as np
 
 
 class Node:
+    """
+    Parameters:
+        tau - ATE (Average Treatment Effect)
+    """
     def __init__(self, tau):
         self.tau = tau
         self.feature_index = None
@@ -33,7 +37,7 @@ class UpliftTreeRegressor:
         self.min_samples_leaf_control = min_samples_leaf_control
         self.tree_ = None
 
-    def _get_thresholds(self, data) -> np.array:
+    def _get_thresholds(self, data: np.array) -> np.array:
         unique_values = np.unique(data)
         if len(unique_values) > 10:
             percentiles = np.percentile(data, [3, 5, 10, 20, 30, 50, 70, 80, 90, 95, 97])
@@ -42,24 +46,24 @@ class UpliftTreeRegressor:
 
         return np.unique(percentiles)
 
-    def _calc_tau(self, y, t) -> float:
+    def _calc_tau(self, y: np.array, t: np.array) -> float:
+        """Calculate ATE (Average Treatment Effect)"""
         return y[t == 1].mean() - y[t == 0].mean()
 
-    def _split_not_valid(self, t):
+    def _split_not_valid(self, t: np.array) -> bool:
         check_ = (
                 (len(t) < self.min_samples_leaf)
-                or (sum(t == 1) < self.min_samples_leaf_treated)
-                or (sum(t == 0) < self.min_samples_leaf_control)
+                or ((t == 1).sum() < self.min_samples_leaf_treated)
+                or ((t == 0).sum() < self.min_samples_leaf_control)
         )
         return check_
 
-    def _best_split(self, x, t, y):
+    def _split(self, x: np.array, t: np.array, y: np.array) -> Tuple:
         delta_p_opt = -1
         best_split = (-1, None, None, None, None, None, None, None)
         for feat_ind in range(x.shape[1]):
             for threshold in self._get_thresholds(x[:, feat_ind]):
                 mask_left = (x[:, feat_ind] <= threshold)
-
                 t_left, t_right = t[mask_left], t[~mask_left]
                 if self._split_not_valid(t_left) or self._split_not_valid(t_right):
                     continue
@@ -75,11 +79,11 @@ class UpliftTreeRegressor:
 
         return best_split
 
-    def _grow_tree(self, x, t, y, depth: int = 0) -> Node:
+    def _grow_tree(self, x: np.array, t: np.array, y: np.array, depth: int = 0) -> Node:
         node = Node(tau=self._calc_tau(y, t))
 
         if depth < self.max_depth:
-            feat_ind, threshold, x_left, t_left, y_left, x_right, t_right, y_right = self._best_split(x, t, y)
+            feat_ind, threshold, x_left, t_left, y_left, x_right, t_right, y_right = self._split(x, t, y)
             if feat_ind >= 0:
                 node.feature_index = feat_ind
                 node.threshold = threshold
@@ -89,7 +93,6 @@ class UpliftTreeRegressor:
         return node
 
     def _predict(self, inputs: np.array) -> float:
-        """Predict class for a single sample."""
         node = self.tree_
         while node.left:
             if inputs[node.feature_index] <= node.threshold:
@@ -110,6 +113,6 @@ class UpliftTreeRegressor:
 
     def predict(self, X: np.ndarray) -> Iterable[float]:
         if not self.tree_:
-            raise ValueError(f'Error. {self.__class__.__name__} is not fitted. Please use .fit() first.')
+            raise ValueError(f'{self.__class__.__name__} is not fitted. Please use .fit() first.')
 
         return [self._predict(inputs) for inputs in X]
